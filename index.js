613c2f7b3a97883ae2c5a291aa920ddcd62b03c3
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-01-16 15:03:13 +0100

    Scraping done, parallel calls started

--- index.js
+++ index.js
@@ -1,46 +1,95 @@
 const puppeteer = require('puppeteer'),
-      URL = 'http://localhost/RetD/bankin-web-scraping-challenge/original-src/', // 'https://web.bankin.com/challenge/index.html'
+      URL = 'http://localhost/TMP/bankin-web-scraping-challenge/original-src/', // 'https://web.bankin.com/challenge/index.html'
       MAX_PAGINATION = 5000,
-      PAGINATION_INCREMENT = 50;
+      PAGINATION_INCREMENT = 50,
+      DATA = [],
+      ITERATOR = [];
+
+for (let i = 0; i < 2; i++) {
+    ITERATOR.push(i * 50);
+}
+
+console.log(ITERATOR);
 
 async function run() {
 
     let startTime = (new Date()).getTime();
 
-    const browser = await puppeteer.launch({
-        headless: true
+    const browserPromise = await puppeteer.launch({
+        headless: false
     });
 
-    const page = await browser.newPage();
+    async function scrapPage(iteration) {
 
-    page.on('dialog', async dialog => {
-        console.log(dialog.message());
-        await dialog.dismiss();
-    });
+        const browser = await browserPromise;
 
-    await page.goto(URL, {
-        waitUntil : 'domcontentloaded'
-    });
+        const page = await browser.newPage();
 
-    const result = await page.evaluate('doGenerate()');
-    console.log(result);
+        page.on('dialog', async dialog => {
+            console.log('> dialog');        
+            await dialog.dismiss();
+            await page.click('#btnGenerate');
+        });
 
-    await page.waitFor('#dvTable');
+        await page.goto(`${URL}?start=${iteration}`, {
+            waitUntil : 'domcontentloaded'
+        });
+    
+        const watchDog = page.waitForFunction(`(Object.keys(document.querySelectorAll('#dvTable tr')).length > 0) || (document.getElementById('fm') !== null && Object.keys(document.getElementById('fm').contentWindow.document.body.querySelectorAll('table tr')).length > 0)`, {
+            polling: 10
+        });
+    
+        await watchDog;
+    
+        let dvTableElements = await page.evaluate((sel) => {
+            return document.querySelectorAll(sel);
+        }, '#dvTable tr');
+    
+        console.log('dvTableElements: ', Object.keys(dvTableElements).length);
 
-    let dvTableElements = await page.evaluate((sel) => {
-        return document.querySelectorAll(sel);
-    }, '#dvTable tr');
+        if (Object.keys(dvTableElements).length > 0) {
+            delete dvTableElements[0];
+            DATA.push({
+                iteration: iteration,
+                data: dvTableElements
+            });
+        }
+    
+        // TODO virer element 1
+    
+        let dvIframeTableElements = await page.evaluate(() => {
+            let result = [];
+            if (document.getElementById('fm')) {
+                result = document.getElementById('fm').contentWindow.document.body.querySelectorAll('table tr');
+            }
+            return result;
+        });
+    
+        console.log('dvIframeTableElements: ', Object.keys(dvIframeTableElements).length);
 
-    console.log(dvTableElements);
+        if (Object.keys(dvIframeTableElements).length > 0) {
+            delete dvIframeTableElements[0];
+            DATA.push({
+                iteration: iteration,
+                data: dvIframeTableElements
+            });
+        }
+    }   
 
-    let finalTime = (new Date()).getTime();
+    async function processPages(iterations) {
+        const promises = iterations.map(scrapPage);
+        await Promise.all(promises);
 
-    console.log(`Scraping done in ${Math.floor((finalTime - startTime) / 1000)} seconds`);
+        let finalTime = (new Date()).getTime();
 
-    //await page.waitFor('#dvTable');
-    //await page.waitFor('#fm');
+        console.log(DATA);        
 
-    browser.close();
+        console.log(`Scraping done in ${Math.floor((finalTime - startTime))} mseconds`); // /1000 seconds
+
+        //browserPromise.close();
+    }
+
+    processPages(ITERATOR);
 }
 
 run();
